7ac7c8c680ddd3d7af8100a3dbecdd2a1ce3a846
Ivelin Peychev <Ivelin Peychev>
2017-04-03 23:07:36 +0200

    Add project files.

---
+++ README.md
@@ -0,0 +1,26 @@
+#Introduction
+TODO: Simple steganography tool, that allows you to quickly pick an image file and an archive and binary copy them into a resulting image that has the contents of the archive hidden inside. Later you can open the image with archiving software (e.g. 7-zip) and extract the hidden file(s). 
+
+#Getting Started
+TODO: Guide users through getting your code up and running on their own system. In this section you can talk about:
+1.	Installation process
+    It's a simple executable. So no installation needed ;)
+2.	Software dependencies
+    Honestly, I've no idea. Probably .net framework 4+
+3.	Latest releases
+    I just started it so 0.0.1.0 I think.
+4.	API references
+    Don't really know what this means. Yes, I'm a noob.
+#Build and Test
+TODO: Describe and show how to build your code and run the tests.
+    If I can build it I doubt you won't be able to. 
+
+#Contribute
+TODO: Explain how other users and developers can contribute to make your code better.
+    If this repo is public, because I have no idea there are no helpful tips for noobs in this whole system.
+    So...if it is public, whoever can contribute with some helpful steganography and cryptography code snippets done in C#, it will be highly appreciated. 
+
+If you want to learn more about creating good readme files then refer the following [guidelines](https://www.visualstudio.com/en-us/docs/git/create-a-readme). You can also seek inspiration from the below readme files:
+- [ASP.NET Core](https://github.com/aspnet/Home)
+- [Visual Studio Code](https://github.com/Microsoft/vscode)
+- [Chakra Core](https://github.com/Microsoft/ChakraCore)
---
+++ stegIT/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace stegIT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string img;
+        public string arch;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".png";
+            dlg.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif|MP3 Files (*.mp3)|*.mp3";
+
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // set the value of 'img' to the full path of the image 
+                img = dlg.FileName;
+                textBox1.Text = "Loaded";
+                //enabling the button to select archive
+                button2.IsEnabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".7z";
+            dlg.Filter = "ZIP Files (*.zip)|*.zip|7zip Files (*.7z)|*.7z|RAR Files (*.rar)|*.rar|TAR Files (*.tar)|*.tar";
+
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // set the value of 'arch' to the full path of the archive 
+                arch = dlg.FileName;
+                textBox2.Text = "Loaded";
+                //enabling the button to call CombineFiles function.
+                button3.IsEnabled = true;
+            }
+        }
+
+        //Clicking on StegIT! button
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            //calls the CombineFiles function using the paths of the image and the archive
+            CombineFiles(img, arch);
+            //Showing a message box saying DONE
+            MessageBox.Show("Done!");
+            //Terminating the application after clicking OK
+            Close();
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        
+
+        private void textBox2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        // Function that BinaryCopies the files (called Streaming apparently)
+        private void CombineFiles(string imgFileName, string archFileName)
+        {
+            using (Stream original = new FileStream(imgFileName, FileMode.Append))
+            {
+                using (Stream extra = new FileStream(archFileName, FileMode.Open, FileAccess.Read))
+                {
+                    var buffer = new byte[32 * 1024];
+
+                    int blockSize;
+                    while ((blockSize = extra.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        original.Write(buffer, 0, blockSize);
+                    }
+                }
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            AboutBox1 aboutWindow = new AboutBox1();
+            aboutWindow.Show();
+        }
+    }
+}
